53ca6bd2de9b482ec5959c7daacccbed29a426e1
Ken Wu <kenqiwu@gmail.com>
2017-04-04 18:42:48 -0500

    Finished. Have to fix formatting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import httplib2
 from apiclient.discovery import build
 from apiclient.errors import HttpError
 from oauth2client.tools import argparser
@@ -13,108 +14,127 @@ import webbrowser
 DEVELOPER_KEY = "AIzaSyB7lBnccMIJwLQJWVtNcdqWo4KtBCDQOzk"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-# old client id: 943664900152-7ghb5t5pmh8c9bueoo23bkmutk1vptge.apps.googleusercontent.com
-# old secret: wENVvmv0uSErthc219GxssgS
 
-# query = input("Search: ")
-# username = input("Google Username: ")
-# password = input("Google Password: ")
+# creates a flow. flow is an object Google uses to manage API requests
+server_flow = client.flow_from_clientsecrets('client_secret.json', scope='https://www.googleapis.com/auth/youtube.readonly',redirect_uri='urn:ietf:wg:oauth:2.0:oob')
+server_flow.params['access_type'] = 'offline'         # offline access
+server_flow.params['include_granted_scopes'] = 'true'   # incremental auth
 
-# webbrowser.open("https://accounts.google.com/o/oauth2/auth?client_id=943664900152-7ghb5t5pmh8c9bueoo23bkmutk1vptge.apps.googleusercontent.com&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=https://gdata.youtube.com&response_type=code")
-# webbrowser.get()
-# user_auth_code = input("Paste your code: ")
-
-secrets_flow = client.flow_from_clientsecrets('client_secret.json', scope='https://www.googleapis.com/auth/youtube.readonly',redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-secrets_flow.params['access_type'] = 'offline'         # offline access
-secrets_flow.params['include_granted_scopes'] = True   # incremental auth
-
-server_flow = OAuth2WebServerFlow(client_id='943664900152-7ghb5t5pmh8c9bueoo23bkmutk1vptge.apps.googleusercontent.com',
-                                  client_secret='wENVvmv0uSErthc219GxssgS',
-                                  scope='https://www.googleapis.com/auth/youtube.readonly',
-                                  redirect_uri='urn:ietf:wg:oauth:2.0:oob')
-
+# authorizes app to request API for user without user needing to get a key
 auth_uri = server_flow.step1_get_authorize_url()
 webbrowser.open(auth_uri)
 webbrowser.get()
 user_auth_code = input("Paste your code: ")
 
+# turns user's auth code into credentials we can use on their behalf
+# refresh token is used to get new credentials without prompting for user auth again
+# flow automatically sends refresh tokens as long as app runs & user access not revoked
 credentials = server_flow.step2_exchange(user_auth_code)
+http_auth = credentials.authorize(httplib2.Http())
 
-# data = {'code': user_auth_code + '&'+
-#         'client_id=943664900152-7ghb5t5pmh8c9bueoo23bkmutk1vptge.apps.googleusercontent.com&' +
-#         'client_secret=wENVvmv0uSErthc219GxssgS&' +
-#         'redirect_uri=http://localhost/oauth2callback&' +
-#         'grant_type=authorization_code'}
+# builds the youtube object with all proper authentication
+# this is used for all API calls
+tube = build('youtube', 'v3', http=http_auth)
 
-# r = requests.post('http://www.googleapis.com/oauth2/v4/token', data)
-# print(r.status_code)
-# print(r.reason)
-
-# r = requests.post('gdata.youtube.com', code='4/XaJRwp3icDTqSI7gn3VRfFbr6Zr_XGbA3-G3Kbzmku8',
-#                  client_id='943664900152-7ghb5t5pmh8c9bueoo23bkmutk1vptge.apps.googleusercontent.com',
-#                  client_secret ='wENVvmv0uSErthc219GxssgS',
-#                  redirect_uri='http://localhost/oauth2callback',
-#                  grant_type='authorization_code')
-# r.json()
-
-channel_id = "UCq6VFHwMzcMXbuKyG7SQYIg"
-query = "ghost in the shell"
-
-# usable after we integrate OAUTH2
-# def get_your_subscriptions(service, part, mine):
-#   service = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
-#
-#   results = service.subscriptions().list(
-#     mine=mine,
-#     part=part
-#   ).execute()
-#
-#   print(results)
-#   pass
+# channel_id should be removed in production
+# channel_id = "UCq6VFHwMzcMXbuKyG7SQYIg"
+# channel_id = "UC2C_jShtL725hvbm1arSV9w"
+query = "money"
+# query = input("Search: ")
 
 
-def youtube_search(options):
-    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+def print_results(results):
+    for item in results.get("items", []):
+        snippet_properties = ['title', 'channelId']
+        channelid = item['snippet']['resourceId']['channelId']
+        print(channelid)
 
-    # Call the search.list method to retrieve results matching the specified
-    # query term.
-    search_response = youtube.search().list(
-        q=options.q,
-        part="id,snippet",
-        maxResults=options.max_results,
-        channelId=options.channel_id
+
+def get_your_subscriptions():
+    results = tube.subscriptions().list(
+    mine='true',
+    part='snippet'
     ).execute()
 
-    videos = []
-    channels = []
-    playlists = []
+    # channel_ids = []
+    channel_ids = {}
 
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
-    for search_result in search_response.get("items", []):
-        if search_result["id"]["kind"] == "youtube#video":
-            videos.append("%s (https://www.youtube.com/watch?v=%s)" % (search_result["snippet"]["title"],
-                                                                       search_result["id"]["videoId"]))
-        elif search_result["id"]["kind"] == "youtube#channel":
-            channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                         search_result["id"]["channelId"]))
-        elif search_result["id"]["kind"] == "youtube#playlist":
-            playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                          search_result["id"]["playlistId"]))
+    for item in results.get("items", []):
+        channel_title = item['snippet']['title']
+        channel_id = item['snippet']['resourceId']['channelId']
+        channel_ids[channel_id] = channel_title
 
-    print("Videos:\n", "\n".join(videos), "\n")
-    print("Channels:\n", "\n".join(channels), "\n")
-    print("Playlists:\n", "\n".join(playlists), "\n")
+    print(len(channel_ids))
+    print(channel_ids)
+    return channel_ids
+
+
+# def youtube_single_search():
+#     search_response = tube.search().list(
+#         q=query,
+#         part="id,snippet",
+#         maxResults='3',
+#         channelId=channel_id
+#     ).execute()
+#
+#     videos = []
+#     channels = []
+#     playlists = []
+#
+#     # Add each result to the appropriate list, and then display the lists of
+#     # matching videos, channels, and playlists.
+#     for search_result in search_response.get("items", []):
+#         if search_result["id"]["kind"] == "youtube#video":
+#             videos.append("%s (https://www.youtube.com/watch?v=%s)" % (search_result["snippet"]["title"],
+#                                                                        search_result["id"]["videoId"]))
+#         elif search_result["id"]["kind"] == "youtube#channel":
+#             channels.append("%s (%s)" % (search_result["snippet"]["title"],
+#                                          search_result["id"]["channelId"]))
+#         elif search_result["id"]["kind"] == "youtube#playlist":
+#             playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+#                                           search_result["id"]["playlistId"]))
+#
+#     print("Videos:\n", "\n".join(videos), "\n")
+#     print("Channels:\n", "\n".join(channels), "\n")
+#     print("Playlists:\n", "\n".join(playlists), "\n")
+
+
+def youtube_search(channels):
+    for channel_id in channels.keys():
+        search_response = tube.search().list(
+            q=query,
+            part="id,snippet",
+            maxResults='3',
+            channelId=channel_id
+        ).execute()
+
+        videos = []
+        channels = []
+        playlists = []
+
+        # Add each result to the appropriate list, and then display the lists of
+        # matching videos, channels, and playlists.
+        for search_result in search_response.get("items", []):
+            if search_result["id"]["kind"] == "youtube#video":
+                videos.append("%s (https://www.youtube.com/watch?v=%s)" % (search_result["snippet"]["title"],
+                                                                           search_result["id"]["videoId"]))
+            elif search_result["id"]["kind"] == "youtube#channel":
+                channels.append("%s (%s)" % (search_result["snippet"]["title"],
+                                             search_result["id"]["channelId"]))
+            elif search_result["id"]["kind"] == "youtube#playlist":
+                playlists.append("%s (%s)" % (search_result["snippet"]["title"],
+                                              search_result["id"]["playlistId"]))
+
+        print(channel_id)
+        print("Videos:\n", "\n".join(videos), "\n")
+        print("Channels:\n", "\n".join(channels), "\n")
+        print("Playlists:\n", "\n".join(playlists), "\n")
 
 
 if __name__ == "__main__":
-    argparser.add_argument("--channel-id", help="Channel ID", default=channel_id)
-    argparser.add_argument("--q", help="Search term", default=query)
-    argparser.add_argument("--max-results", help="Max results", default=25)
-    args = argparser.parse_args()
-
     try:
-        # get_your_subscriptions(args, 'snippet,contentDetails', True)
-        youtube_search(args)
+        channels = get_your_subscriptions()
+        youtube_search(channels)
+        # youtube_single_search()
     except HttpError:
         print("An HTTP error occurred:\n%s" % HttpError.content)
