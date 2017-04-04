3f40f725537af8b68ac62c5700e5f7e14d969fcc
Ken Wu <kenqiwu@gmail.com>
2017-04-03 21:58:49 -0500

    Comment outline

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 # password = input("Password: ")
 
 username = "kenqiwu@gmail.com"
-password = "uhjoi"
+password = "pass"
 
 # can save sign in credentials to login.properties
 
 # put credentials into base API call
 
 query = input("Search: ")
-
 
 # returns 10 videos with Title, and URL and channel, and amtViews
