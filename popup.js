9f0e82b83bf7d4778c8895181f54cd3fe71aa741
kenwuuu <kenwuuu@users.noreply.github.com>
2017-06-21 14:32:44 -0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -70,28 +70,36 @@ function searchAllSubs(query, channel) {
 			//alert(videoCount);
 			var count = 0;
 			
+			var channelContainer = document.createElement('div');
 			var channelTitlePlace = document.createElement('h2');
 			var channelTitle = document.createTextNode(videosParsed.items[0].snippet.channelTitle);
 			channelTitlePlace.appendChild(channelTitle);
-			document.getElementById('display').appendChild(channelTitlePlace);
+			channelContainer.appendChild(channelTitlePlace);
 			
 			//document.getElementById('display').innerHTML = document.getElementById('display').innerHTML + 
 			//'<br />===' + videosParsed.items[0].snippet.channelTitle + '===<br />';
 			
 			while (count < videoCount) {
+				var videoContainer = document.createElement('div');
+				var imageContainer = document.createElement('div');
+				var textContainer = document.createElement('div');
 				
 				//create image
 				var thumb = document.createElement('img');
 				thumb.src = videosParsed.items[count].snippet.thumbnails.default.url;
 				thumb.align = 'top';
+				thumb.style.cssFloat = "left";
 				thumb.style.marginBottom = "10px";
 				thumb.style.marginRight = "10px";
-				document.getElementById('display').appendChild(thumb);
+				imageContainer.appendChild(thumb);
 				
 				//create title text
+				var infoBox = document.createElement('div');
+				infoBox.style.cssFloat = "left";
 				var videoTitlePlace = document.createElement('a');
 				videoTitlePlace.appendChild(document.createTextNode(videosParsed.items[count].snippet.title));
-				document.getElementById('display').appendChild(videoTitlePlace);
+				infoBox.appendChild(videoTitlePlace);
+				textContainer.appendChild(infoBox);
 				
 				//create link and add to image and title
 				var att = document.createAttribute('href');
@@ -99,6 +107,13 @@ function searchAllSubs(query, channel) {
 				videoTitlePlace.setAttributeNode(att);
 				thumb.href = att;
 				
+				videoContainer.appendChild(imageContainer);
+				videoContainer.appendChild(textContainer);
+				channelContainer.appendChild(videoContainer);
+				document.getElementById('display').appendChild(channelContainer);
+				
+				thumb.style.clear = 'both';
+				infoBox.style.clear = 'both';
 				//document.getElementById('display').appendChild(document.createElement("BR"));
 				
 				//document.getElementById('display').innerHTML = document.getElementById('display').innerHTML +
